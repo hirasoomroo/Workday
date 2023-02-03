f15d6a4e3cc43efc5066ae720c24e48c178b76eb
Hira Soomro <hira@Hiras-MacBook-Pro.local>
2023-02-03 08:33:11 -0800

    updates

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//display today's date
 var todayDate = moment().format('dddd, MMM Do YYYY');
+
+var saveButton = document.querySelector("#savebutton");
 $("#currentDay").html(todayDate);
 $(function () {
 });
+//Time array for blocks of time listed
+var timeArray = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var currentTime = [];
 
 function timeTracker() {
     //get current number of hours.
     var thisMoment = moment().hour();
 }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
-  
- 
+//create click button for saving 
+function saveButton() {}
+saveButton.addEventListener ("click", function() {
+})
 
-
-
-  //HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //use storage to save button with the to do list
+  $("btn saveBtn col-2 col-md-1").on("click", function(){
+    $('input, select, textarea').each(function() {
+     var value = $(this).val(),
+         name = $(this).attr('name');
+     localStorage[name] = value;
+             
+})});
